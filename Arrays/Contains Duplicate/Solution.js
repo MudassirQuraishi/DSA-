a6ff9e432c3e0bcd620456d75b4129dfb3dcc97a
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-06-09 23:24:53 +0530

    Contains Duplicate

---
+++ Arrays/Contains Duplicate/Solution.js
@@ -0,0 +1,11 @@
+let nums = [1,2,3,1];
+function containsDuplicate(nums){
+    nums.sort(function(a,b){return a-b});
+    for(let i =0;i<nums.length-1;i++){
+        if(nums[i]==nums[i+1]){
+            return true
+        }
+    }
+    return false;
+}
+console.log(containsDuplicate(nums));
