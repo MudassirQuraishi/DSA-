785af988581e1ee51d588d5ed8b75fac27e94e7d
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-06-14 16:35:38 +0530

    Build Array

---
+++ Arrays/Build Array/Solution.js
@@ -0,0 +1,14 @@
+
+function buildArray(nums) {
+    let n = nums.length;
+    let ans =[];
+    for(let i=0;i<n;i++){
+        ans[i]=nums[nums[i]];
+    }
+    return ans;
+    
+}
+
+let nums = [0,2,1,5,3,4];
+let Array = buildArray();
+console.log(Array);
